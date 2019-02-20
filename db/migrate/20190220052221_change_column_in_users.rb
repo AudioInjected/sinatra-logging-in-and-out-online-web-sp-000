6af730b5dac948354dad20ee3a6ae47af9e22c9e
Rafael Batista <rafaelb24@yahoo.com>
2019-02-20 05:32:27 +0000

    Automatically backed up by Learn

--- db/migrate/20190220052221_change_column_in_users.rb
+++ db/migrate/20190220052221_change_column_in_users.rb
@@ -1,6 +1,7 @@
 class ChangeColumnInUsers < ActiveRecord::Migration[5.1]
   def change
-    change_column :users, :balance, :float
+    change_table :users do |t| 
+    t.change :balance, :float
   end
 end
 
